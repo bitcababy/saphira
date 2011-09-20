ca8458c77df4b8a35b8613b14d6561917ba7187c
Paul Spieker <p.spieker@duenos.de>
2011-09-20 11:38:48 +0200

    bugfix

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,6 +1,8 @@
 require 'dragonfly/rails/images'
 
-app = Dragonfly[:images]
+DRAGONFLY_APP = Dragonfly[:images]
+
+app = DRAGONFLY_APP
 app.datastore.root_path = ::Rails.root.join('system/files', ::Rails.env).to_s
 app.job :thumb_square do |size|
   process :thumb, "#{size}x#{size}#"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Saphira::Engine.routes.draw do
   resources :image_variants
-
   resources :file_items, :path => '/files', :id => /[^\\.]+/ do
     resources :image_variant_settings
   end
